7655d5bb6db4bd69b1d57678aa609004aa1d425e
Oleg Narbut <Olegatorium@gmail.com>
2023-11-27 06:52:02 +0100

    Ready

---
+++ HomeworkWeatherApp-UsingDependencyInjection/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using HomeworkWeatherApp.Models;
+using IServiceContracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeworkWeatherApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWeatherService _weatherService;
+
+        private readonly IWeatherService _serviceFindCity;
+
+
+		public HomeController(IWeatherService WeatherService, IWeatherService ServiceFindCity) 
+        {
+           _weatherService= WeatherService;
+
+           _serviceFindCity = ServiceFindCity;
+        }
+
+		[Route("/")]
+        public IActionResult Index()
+        {
+			List<CityWeather> citiesWeather = _weatherService.GetWeatherDetails();
+
+
+			return View(citiesWeather);
+        }
+
+        [Route("/weather/{cityCode}")]
+
+        public IActionResult Details(string? cityCode)
+        {
+			CityWeather? matchingCity = _serviceFindCity.GetWeatherByCityCode(cityCode);
+
+            if (matchingCity == null)
+            {
+                return BadRequest("You write not exist Unique Code:( ");
+            }
+
+			return View(matchingCity);
+		}
+    }
+}
